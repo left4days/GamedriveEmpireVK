bbbdd095251d9a6a7325e9cf05796600d7281ff5
Kobernik.EV <Kobernik.EV@parma.ru>
2020-02-13 21:47:00 +0100

    new build

---
+++ build/precache-manifest.579b44761ddfb84ed799e58044bf425e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "76e355112501578b99694a3d4baeef05",
+    "url": "/static/media/image_2block_gamedrive.76e35511.png"
+  },
+  {
+    "revision": "45054620b4e4808d9731",
+    "url": "/static/js/main.5f9e9e59.chunk.js"
+  },
+  {
+    "revision": "6b8c96d54949d295b8d033404db30592",
+    "url": "/static/media/image_3block_brush.6b8c96d5.png"
+  },
+  {
+    "revision": "5820690b1c4be22efca0d696961a04dc",
+    "url": "/static/media/1.5820690b.svg"
+  },
+  {
+    "revision": "29b6eab288e7c1085e2e4414e28a2f92",
+    "url": "/static/media/2.29b6eab2.svg"
+  },
+  {
+    "revision": "f0cd955f7ae4bc02cd55fc5cd3bdbf54",
+    "url": "/static/media/3.f0cd955f.png"
+  },
+  {
+    "revision": "d382b030853ea3fe75188bd8973aac1c",
+    "url": "/static/media/gift.d382b030.png"
+  },
+  {
+    "revision": "10a3c0aa0fd26656e7668c1bc6919a8c",
+    "url": "/static/media/product_mobile.10a3c0aa.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "9ebdca618a5d5cef6faa4bc9fccc343e",
+    "url": "/static/media/image_3block_disk2.9ebdca61.png"
+  },
+  {
+    "revision": "6b8f28aa6f2a6c92b40ecff981d0ac26",
+    "url": "/static/media/image_3block_decor.6b8f28aa.png"
+  },
+  {
+    "revision": "154ed11357b9820e8a811167247a80a9",
+    "url": "/static/media/logo_seagate.154ed113.svg"
+  },
+  {
+    "revision": "7b863562aeed73f77ea90aa363e7cf83",
+    "url": "/static/media/logo_impire.7b863562.svg"
+  },
+  {
+    "revision": "0183dfac86e3cd801ed1e4bddf1fd8fa",
+    "url": "/static/media/image_1block_brush.0183dfac.png"
+  },
+  {
+    "revision": "9342634c8e1e335134ce7de418b69976",
+    "url": "/static/media/image_1block_disk.9342634c.png"
+  },
+  {
+    "revision": "cad9e1cf613956d75d4f",
+    "url": "/static/js/2.9ebdb058.chunk.js"
+  },
+  {
+    "revision": "d83f100422a998ab953d27b9441f0f97",
+    "url": "/index.html"
+  }
+];
